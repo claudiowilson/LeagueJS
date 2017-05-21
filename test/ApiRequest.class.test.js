2c41f01d90db1b6a0e03676bdd6f2a12f18d63a5
Jonas Krispin <jonas@krispin.it>
2017-05-21 19:29:55 +0200

    refactor (ApiRequest) make execute static with requiring an url

--- test/ApiRequest.class.test.js
+++ test/ApiRequest.class.test.js
@@ -5,62 +5,54 @@ describe('Endpoint Testsuite', function () {
 	const chaiAsPromised = require("chai-as-promised");
 	const should = chai.should;
 	const expect = chai.expect;
-	// const expect = chai.expect;
 	chai.use(chaiAsPromised);
 	chai.use(should);
 
-	const ApiRequest = require('../lib/apiRequest');
+	const ApiRequest = require('../lib/ApiRequest.class');
 
 
 	const mock_urlWithJSONResponse = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion/Aatrox.json';
 	const mock_urlWithNonJSONResponse = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg';
 	const mock_urlWith404 = 'http://ddragon.leagueoflegends.com';
 
-	describe('constructor', function () {
-		it('needs an url', function () {
-			expect(()=>{new ApiRequest();}).to.throw(require('../lib/errors/ParameterError'));
-			expect(()=>{new ApiRequest(mock_urlWithJSONResponse);}).not.to.throw(require('../lib/errors/ParameterError'));
-		});
+
+	it('needs an url to execute', function () {
+		expect(() => {ApiRequest.execute();}).to.throw(require('../lib/errors/ParameterError'));
+		expect(() => {ApiRequest.execute(mock_urlWithJSONResponse);}).not.to.throw(require('../lib/errors/ParameterError'));
 	});
+
 	describe('valid requests', function () {
 		it('are able to make requests to a given (valid) url', function () {
-			const request = new ApiRequest(mock_urlWithJSONResponse);
-			return request.execute().should.eventually.be.fulfilled;
+			return ApiRequest.execute(mock_urlWithJSONResponse).should.eventually.be.fulfilled;
 		});
 		it('will parse JSON responsens', function () {
-			const request = new ApiRequest(mock_urlWithJSONResponse);
-			return request.execute().should.eventually.have.property('data');
+			return ApiRequest.execute(mock_urlWithJSONResponse).should.eventually.have.property('data');
 		});
 		it('will provide the body for responses that are not JSON', function () {
-			const request = new ApiRequest(mock_urlWithNonJSONResponse);
-			return request.execute().should.eventually.be.fulfilled.and.be.a('string');
+			return ApiRequest.execute(mock_urlWithNonJSONResponse).should.eventually.be.fulfilled.and.be.a('string');
 		});
 	});
 
 	describe('unsuccesful requests', function () {
 		it('rejects with an elaborate error object', function () {
-			const request = new ApiRequest(mock_urlWith404);
-			return request.execute().should.eventually.be.rejectedWith({
+			return ApiRequest.execute(mock_urlWith404).should.eventually.be.rejectedWith({
 				name: 'StatusCodeError',
 				statusCode: 404
 			}).and.have.property('message');
 		});
 		it('rejects with the full response', function () {
-			const request = new ApiRequest(mock_urlWith404);
-			return request.execute().should.eventually.be.rejected.and.have.property('response');
+			return ApiRequest.execute(mock_urlWith404).should.eventually.be.rejected.and.have.property('response');
 		});
 
 		it('rejects with the options set for the request', function () {
-			const request = new ApiRequest(mock_urlWith404);
-			return request.execute().should.eventually.be.rejected.and.have.property('options')
+			return ApiRequest.execute(mock_urlWith404).should.eventually.be.rejected.and.have.property('options')
 				.and.that.to.have.property('url').equals(mock_urlWith404);
 		});
 	});
 
 	describe('options', function () {
 		it('execute() can return the full response from the request', function () {
-			const request = new ApiRequest(mock_urlWithJSONResponse);
-			return request.execute(true).should.eventually.be.an('object').and.have.property('headers');
+			return ApiRequest.execute(mock_urlWithJSONResponse, true).should.eventually.be.an('object').and.have.property('headers');
 		});
 	});
 
