2e91885dab94a0b277d458b0e0c2d741883d2db0
Jonas Krispin <jonas@krispin.it>
2017-05-26 15:53:14 +0200

    feat (LolStatusEndpoint) add endpoint

---
+++ test/endpoints/LolStatusEndpoint.test.js
@@ -0,0 +1,25 @@
+describe('ChampionEndpoint Testsuite', function () {
+	'use strict';
+
+	const LolStatusEndpoint = require('../../lib/endpoints/LolStatusEndpoint');
+
+	const chai = require("chai");
+	const chaiAsPromised = require("chai-as-promised");
+	const should = chai.should;
+	const expect = chai.expect;
+	chai.use(chaiAsPromised);
+	chai.use(should);
+
+	const TestUtil = require('../TestUtil');
+	let mergedConfig = TestUtil.getTestConfig();
+
+
+	let endpoint;
+	beforeEach(function () {
+		endpoint = new LolStatusEndpoint(mergedConfig, ['euw1']);
+	});
+
+	it('gets the status of the shard specified by the platformId', function () {
+		return endpoint.gettingShardData('na1').should.eventually.have.property('slug').equal('na');
+	});
+});
