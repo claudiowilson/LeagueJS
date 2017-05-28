ed5da324dec1ea6337a481458b6c50ece40e9980
Jonas Krispin <jonas@krispin.it>
2017-05-28 12:20:00 +0200

    chores (cleanup project) * remove test/testConfig.json and regenerate DEV API-KEY * remove project file njsproj * update dependencies * update gulpfile * update sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,93 @@
 /*global require*/
 
 (function () {
-    'use strict';
+	'use strict';
 
-    var gulp = require('gulp'),
-        jshint = require('gulp-jshint'),
-        mocha = require('gulp-mocha'),
-        istanbul = require('gulp-istanbul'),
-        clean = require('gulp-clean'),
-        exec = require('gulp-exec'),
-        plato = require('gulp-plato');
+	const gulp = require('gulp'),
+		jshint = require('gulp-jshint'),
+		mocha = require('gulp-mocha'),
+		istanbul = require('gulp-istanbul'),
+		isparta = require('isparta'),
+		clean = require('gulp-clean'),
+		exec = require('gulp-exec'),
+		plato = require('plato');
 
-    gulp.task('lint', function () {
-        gulp.src(['./lib/*', './test/*', './gulpfile.js'])
-            .pipe(jshint('.jshintrc'))
-            .pipe(jshint.reporter('default'));
-    });
+	gulp.task('lint', function () {
+		gulp.src(['./lib/**/*', './test/**/*.test.js', './gulpfile.js'])
+			.pipe(jshint('.jshintrc'))
+			.pipe(jshint.reporter('default'));
+	});
 
-    gulp.task('istanbul', function (callback) {
-        gulp.src('./lib/*.js')
-            .pipe(istanbul())
-            .on('end', callback);
-    });
+	gulp.task('istanbul', function () {
+		return gulp.src(['./lib/**/*.js'])
+			.pipe(istanbul({
+				instrumenter: isparta.Instrumenter,
+				includeUntested: true
+			}))
+			.pipe(istanbul.hookRequire());
+	});
 
-    gulp.task('test', function () {
-        gulp.src('./test/*.test.js').pipe(mocha({
-            reporter: 'spec',
-            bail: true,
-            ui: 'bdd'
-        }));
-    });
+	gulp.task('test', function () {
+		gulp.src('./test/**/*.test.js').pipe(mocha({
+			reporter: 'spec',
+			bail: false,
+			ui: 'bdd'
+		}));
+	});
 
-    gulp.task('test-coverage', function () {
-        gulp.run('istanbul', function () {
-            gulp.src('./test/*.js')
-                .pipe(mocha())
-                .pipe(istanbul.writeReports())
-                .pipe(exec('echo Coverage has been calculated. See coverage directory for details.'));
-        });
-    });
+	// TODO: currently (2017/28/25) broken for es6 on node 5+ see https://github.com/SBoudrias/gulp-istanbul/issues/100
+	gulp.task('test-coverage', ['istanbul'], function () {
+		return gulp.src('./test/**/*.test.js')
+			.pipe(mocha({
+				reporter: 'dot',
+				bail: false,
+				ui: 'bdd'
+			}))
+			.pipe(istanbul.writeReports())
+			.pipe(exec('echo Coverage has been calculated. See coverage directory for details.'));
+	});
 
-    gulp.task('code-report', function () {
-        gulp.src('./lib/*.js')
-            .pipe(plato('./report'))
-            .pipe(exec('echo The code report has been generated. See report directory for details.'));
-    });
+	gulp.task('code-report', function () {
+		return plato.inspect('./lib/**/*.js', './report', {}, () => [
+			exec('echo The code report has been generated. See report directory for details.')
+		]);
+	});
 
-    gulp.task('reports', function () {
-        gulp.run('test-coverage', 'code-report');
-    });
+	gulp.task('reports', ['test-coverage', 'code-report']);
 
-    gulp.task('clean-reports', function () {
-        gulp.src(['./coverage', './report'], {
-            read: false
-        })
-            .pipe(clean({
-                force: true
-            }));
-        gulp.src('').pipe(exec('echo Coverage ^& Report directories has been removed'));
-    });
+	gulp.task('clean-reports', function () {
+		gulp.src(['./coverage', './report'], {
+			read: false
+		})
+			.pipe(clean({
+				force: true
+			}));
+		gulp.src('').pipe(exec('echo Coverage ^& Report directories has been removed'));
+	});
 
-    gulp.task('dev', function () {
-        gulp.run('lint', 'test');
-        gulp.watch(['./lib/*', './test/*'], function () {
-            gulp.run('lint', 'test');
-        });
-    });
+	gulp.task('dev', function () {
+		gulp.run('lint', 'test');
+		gulp.watch(['./lib/**/*', './test/**/*'], function () {
+			gulp.run('lint', 'test');
+		});
+	});
 
-    // gulp.task('prod', function () {
-    //     if (gulp.env.major) {
-    //         gulp.run('lint', 'test');
-    //         gulp.src('./package.json')
-    //             .pipe(mversion('major'))
-    //             .pipe(gulp.dest('./'));
-    //     } else if (gulp.env.minor) {
-    //         gulp.run('lint', 'test');
-    //         gulp.src('./package.json')
-    //             .pipe(mversion('minor'))
-    //             .pipe(gulp.dest('./'));
-    //     } else if (gulp.env.patch) {
-    //         gulp.run('lint', 'test');
-    //         gulp.src('./package.json')
-    //             .pipe(mversion('patch'))
-    //             .pipe(gulp.dest('./'));
-    //     }
-    // });
+	// gulp.task('prod', function () {
+	//     if (gulp.env.major) {
+	//         gulp.run('lint', 'test');
+	//         gulp.src('./package.json')
+	//             .pipe(mversion('major'))
+	//             .pipe(gulp.dest('./'));
+	//     } else if (gulp.env.minor) {
+	//         gulp.run('lint', 'test');
+	//         gulp.src('./package.json')
+	//             .pipe(mversion('minor'))
+	//             .pipe(gulp.dest('./'));
+	//     } else if (gulp.env.patch) {
+	//         gulp.run('lint', 'test');
+	//         gulp.src('./package.json')
+	//             .pipe(mversion('patch'))
+	//             .pipe(gulp.dest('./'));
+	//     }
+	// });
 }());
---
+++ sample/sample.js
@@ -0,0 +1,17 @@
+const LeagueJs = require('../lib/LeagueJS.js');
+
+const api = new LeagueJs({
+	API_KEY: 'your-api-key or add to process.env.LEAGUE_API_KEY',
+	PLATFORM_ID: LeagueJs.getPlatformIdForRegion('euw')
+});
+
+api.Summoner
+	.gettingByName('EldoranDev')
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
